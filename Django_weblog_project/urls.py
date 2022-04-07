03ac23f7af5b4c06e2226774c50c06b5393527a9
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-04-07 19:32:17 +0200

    Revert "begin comments"
    This reverts commit 7bd49f83fe717db8577365dc9bf5665369846826.

--- Django_weblog_project/urls.py
+++ Django_weblog_project/urls.py
@@ -19,13 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("accounts.urls")),
-    path("comments/", include("django_comments.urls")),
-    path("", include("posts.urls")),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('posts.urls')),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- README.md
+++ README.md
@@ -5,6 +5,4 @@ pipenv shell
 pipenv install
 
 python3 -m pip install --upgrade Pillow
-
-python3 pip install django-contrib-comments
 ```
